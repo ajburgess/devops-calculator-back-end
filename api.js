df2b8d4ff8008cd524b03a3ad6b45e579378c802
ajburgess <developerandrewburgess@outlook.com>
2020-09-10 21:33:17 +0100

    Fixed port

--- api.js
+++ api.js
@@ -4,13 +4,14 @@ const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.disable('etag');
 app.use(cors());
 
 app.get("/add/:num1/:num2", (req, res) => {
     const num1 = parseFloat(req.params.num1);
     const num2 = parseFloat(req.params.num2);
     const answer = num1 + num2;
-    res.status(200).json(answer);
+    res.json(answer);
 });
 
 module.exports = app;
