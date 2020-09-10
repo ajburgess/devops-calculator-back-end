f2906206fbe25497fa57deb5f65758811a8412c7
ajburgess <developerandrewburgess@outlook.com>
2020-09-10 21:18:09 +0100

    Fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./api');
 
 require('dotenv').config();
-const port = process.env.PORT | 8001;
+const port = process.env.PORT || 8001;
 
 app.listen(port,
     () => console.log(`Listening on port: ${port}...`));
